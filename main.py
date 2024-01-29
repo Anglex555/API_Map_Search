a46112a476066cb9d3dce61a620e0c3b68db9671
Anglex555 <87623102+Anglex555@users.noreply.github.com>
2024-01-29 18:13:45 +0300

    main_commit_3
    hide api

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import map_utils
 search_query = " ".join(sys.argv[1:])
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 geocoder_params = {
-    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+    "apikey": "API_КЛЮЧ",
     "geocode": search_query,
     "format": "json"}
 response = requests.get(geocoder_api_server, params=geocoder_params)
