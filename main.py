fc82d6af26565e9213f4d8611e1c8db9764b1968
Anglex555 <87623102+Anglex555@users.noreply.github.com>
2024-01-29 18:11:57 +0300

    main_commit_2
    add files with changes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import map_utils
 search_query = " ".join(sys.argv[1:])
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 geocoder_params = {
-    "apikey": "API_КЛЮЧ",
+    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
     "geocode": search_query,
     "format": "json"}
 response = requests.get(geocoder_api_server, params=geocoder_params)
@@ -20,5 +20,5 @@ toponym = json_response["response"]["GeoObjectCollection"][
 toponym_coordinates = toponym["Point"]["pos"]
 toponym_longitude, toponym_latitude = toponym_coordinates.split(" ")
 address_ll = ",".join([toponym_longitude, toponym_latitude])
-response = map_utils.get_size_parms(json_response, address_ll)
+response = map_utils.get_size_parms(address_ll)
 Image.open(BytesIO(response.content)).show()
--- map_utils.py
+++ map_utils.py
@@ -1,17 +1,16 @@
 import requests
 
 
-def get_size_parms(json_response, address_ll):
-    org_point = address_ll
-    print("Введите размер")
-    delta1 = input()
-    print("Введите размер")
-    delta2 = input()
+def get_size_parms(address):
+    point = address
+    delta_longitude = input("Введите масштаб долготы: ")
+    delta_latitude = input("Введите масштаб ширины: ")
+
     map_params = {
-        "ll": address_ll,
-        "spn": ",".join([delta1, delta2]),
+        "ll": address,
+        "spn": ",".join([delta_longitude, delta_latitude]),
         "l": "map",
-        "pt": "{},pm2dgl".format(org_point)
+        "pt": "{},pm2dgl".format(point)
     }
 
     map_api_server = "http://static-maps.yandex.ru/1.x/"
